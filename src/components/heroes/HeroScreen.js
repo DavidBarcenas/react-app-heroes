44754c08dd100cd5e25397d48ad3ec94dcaad6e7
David Barcenas <daveepro@outlook.com>
2020-08-04 16:21:32 -0500

    feat(HeroScreen): Read url parameters [useParams, Redirect]

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useParams, Redirect } from 'react-router-dom'
+import { getHeroesById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+  const { id } = useParams();
+  const hero = getHeroesById(id);
+
+  if(!hero) { return <Redirect to='/' /> }
+
   return (
     <div>
-      <h1>HeroScreen</h1>
+      <h1>{hero.superhero}</h1>
     </div>
   )
 }
