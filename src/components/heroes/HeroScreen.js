e1ff47648e6b23a1e9a2611718ae48df94b58971
David Barcenas <daveepro@outlook.com>
2020-08-04 16:31:05 -0500

    feat(HeroScreen): Create back button [history]

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,16 +2,25 @@ import React from 'react'
 import { useParams, Redirect } from 'react-router-dom'
 import { getHeroesById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
 
   const { id } = useParams();
   const hero = getHeroesById(id);
 
   if(!hero) { return <Redirect to='/' /> }
 
+
+  const handleReturn = () => {
+    if(history.length <= 2) {
+      history.push('/')
+    } else {
+      history.goBack();
+    }
+  }
   return (
     <div>
       <h1>{hero.superhero}</h1>
+      <button onClick={handleReturn}>Regresar</button>
     </div>
   )
 }
