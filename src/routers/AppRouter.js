6b407426f1fbcfe45573ee22aa62ad6698c42933
David Barcenas <daveepro@outlook.com>
2020-08-17 20:26:34 -0500

    feat(login): Create private routes [PrivateRoute]

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { LoginScreen } from '../components/login/LoginScreen'
 import { HeroesRoutes } from './HeroesRoutes'
+import { PrivateRoute } from './PrivateRoute'
+import { useContext } from 'react'
+import { AuthContext } from '../auth/AuthContext'
 
 export const AppRouter = () => {
+
+  const {user: {logged}} = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path='/login' component={LoginScreen}/>
-        <Route path='/' component={HeroesRoutes}/>
+        <PrivateRoute path='/' isAuth={logged} component={HeroesRoutes}/>
       </Switch>
     </Router>
   )
