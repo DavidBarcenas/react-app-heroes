78e778cf889f0beef799b4db2937db94c4d3158a
David Barcenas <daveepro@outlook.com>
2020-08-17 19:45:25 -0500

    feat(login): Create logout method [useHistory]

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
 import './navbar.css'
 import { useContext } from 'react'
 import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const Navbar = () => {
 
-  const {user} = useContext(AuthContext)
+  const {user, dispatch} = useContext(AuthContext)
+  const history = useHistory()
+
+  const handleLogout = () => {
+
+    dispatch({
+      type: types.logout
+    })
+
+    history.replace('/login')
+  }
 
   return (
     <nav className="main__nav red">
@@ -17,7 +28,7 @@ export const Navbar = () => {
           <li><NavLink activeClassName="active" to="/dc">DC</NavLink></li>
           <li><NavLink activeClassName="active" to="/search">Search</NavLink></li>
           <li className="activeUser">{user.name}</li>
-          <li><NavLink activeClassName="active" to="/login">Logout</NavLink></li>
+          <li><button className="waves-effect waves-light btn" onClick={handleLogout}>Logout</button></li>
         </ul>
       </div>
     </nav>
